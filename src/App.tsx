fe6fd2dc0989e988c42470d69ae576054a04e705
Ed Tansey <office.fwd@yahoo.com>
2022-01-04 14:18:30 +0000

    Create shopping cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Grid from '@material-ui/core/Grid'
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
 // Styles
-import { Wrapper } from './App.styles'
+import { Wrapper, StyledButton } from './App.styles'
 import { NumberLiteralType } from 'typescript'
 //Types
 export type CartItemType = {
@@ -25,13 +25,15 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json()
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
     getProducts
   )
   console.log(data)
 
-  const getTotalItems = () => null
+  const getTotalItems = (items: CartItemType[]) => null
 
   const handleAddToCart = (clickedItem: CartItemType) => null
 
@@ -42,6 +44,14 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Goes Here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
